b7d7b128ea9debca95f59a6af7d7b72909d7e63c
MathRoberto <mbarcellosferraz@hotmail.com>
2023-09-21 10:04:44 -0300

    aad

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/interface", (req, res) => {
   res.sendFile("Interface.html", { root: interfaceDirectory });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000; // Usará a porta definida no ambiente ou a porta 3000 por padrão
 app.listen(port, () => {
   console.log(`Server iniciado corretamente e hospedado na porta ${port}`);
 });
